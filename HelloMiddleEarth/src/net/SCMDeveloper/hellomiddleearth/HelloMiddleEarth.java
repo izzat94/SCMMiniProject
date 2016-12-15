a843f04aa841570749c9a495522d3a207eba56bf
izzat94 <izzatazmi49@gmail.com>
2016-12-16 06:20:39 +0800

    very

---
+++ HelloMiddleEarth/src/net/SCMDeveloper/hellomiddleearth/HelloMiddleEarth.java
@@ -0,0 +1,15 @@
+package net.SCMDeveloper.hellomiddleearth;
+
+public class HelloMiddleEarth {
+
+	public static void main(String[] args) {
+
+		String[] names = { "Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf", "Hero", "Hola"};
+		
+		// print greetings to the fine folks in the Middle Earth
+			for (String name : names){
+				System.out.format("Hello, %s!\n", name);
+				System.out.println("How are you doing today?");
+			}
+	}
+}
---
+++ HelloSCM/src/net/SCMDeveloper/helloscm/HelloSCM.java
@@ -0,0 +1,11 @@
+package net.SCMDeveloper.helloscm;
+
+public class HelloSCM {
+
+	public static void main(String[] args) {
+		// a greeting
+		System.out.println("Hello, git!");
+
+	}
+
+}
