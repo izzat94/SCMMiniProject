17a22d272e8528e9753a533c8c174fa4a05936ee
SCMDeveloper <nizwanmarjugi@gmail.com>
2016-12-15 23:49:55 +0800

    Converted for loop

--- HelloMiddleEarth/src/net/SCMDeveloper/hellomiddleearth/HelloMiddleEarth.java
+++ HelloMiddleEarth/src/net/SCMDeveloper/hellomiddleearth/HelloMiddleEarth.java
@@ -3,8 +3,12 @@ package net.SCMDeveloper.hellomiddleearth;
 public class HelloMiddleEarth {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String[] names = { "Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+		
+		// print greetings to the fine folks in the Middle Earth
+			for (String name : names){
+				System.out.format("Hello, %s!\n", name);
+			}
 	}
-
 }
